45dea0d578b1376619195250f3341a876218c56d
Soyoun Kim <makeajourney@gmail.com>
2021-11-27 15:18:52 +0900

    Initialize the project

---
+++ src/test/kotlin/kr/makeajourney/urlshortener/UrlshortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.makeajourney.urlshortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
